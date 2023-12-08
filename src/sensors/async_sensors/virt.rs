769c823f83f6f81673fd9419938b3cb7bcc7b928
Malte Veerman <malte.veerman@gmail.com>
2023-12-08 04:34:51 +0100

    Added async virtual sensors

--- src/sensors/async_sensors/virt.rs
+++ src/sensors/async_sensors/virt.rs
@@ -1,22 +1,24 @@
 //! Module containing the virtual sensors and their related functionality.
 
-use super::{Error, Result};
+use super::*;
 use crate::units::Raw;
 
 use std::{
     fmt::Debug,
-    fs::read_to_string,
     path::{Path, PathBuf},
 };
 
+use tokio::fs::read_to_string;
+
+#[async_trait]
 /// Helper trait that sums up all functionality of a read-only virtual sensor.
-pub trait VirtualSensor<T: Raw>: std::fmt::Debug {
+pub trait AsyncVirtualSensor<T: Raw>: std::fmt::Debug {
     /// Returns the path to this virtual sensor's underlying file.
     fn path(&self) -> &Path;
 
     /// Reads the virtual sensor.
-    fn read(&self) -> Result<T> {
-        match read_to_string(self.path()) {
+    async fn read(&self) -> Result<T> {
+        match read_to_string(self.path()).await {
             Ok(s) => Ok(T::from_raw(s.trim())?),
             Err(e) => match e.kind() {
                 std::io::ErrorKind::PermissionDenied => Err(Error::InsufficientRights {
@@ -34,18 +36,19 @@ pub(crate) struct VirtualSensorStruct {
     path: PathBuf,
 }
 
-impl<T: Raw> VirtualSensor<T> for VirtualSensorStruct {
+impl<T: Raw> AsyncVirtualSensor<T> for VirtualSensorStruct {
     fn path(&self) -> &Path {
         &self.path
     }
 }
 
 #[cfg(feature = "writeable")]
+#[async_trait]
 /// Helper trait that sums up all functionality of a read-write virtual sensor.
-pub trait WriteableVirtualSensor<T: Raw>: VirtualSensor<T> {
+pub trait AsyncWriteableVirtualSensor<T: Raw + Sync>: AsyncVirtualSensor<T> {
     /// Writes to the virtual sensor.
-    fn write(&self, value: &T) -> Result<()> {
-        std::fs::write(self.path(), value.to_raw().as_bytes()).map_err(|e| match e.kind() {
+    async fn write(&self, value: &T) -> Result<()> {
+        tokio::fs::write(self.path(), value.to_raw().as_bytes()).await.map_err(|e| match e.kind() {
             std::io::ErrorKind::PermissionDenied => Error::insufficient_rights(self.path()),
             _ => Error::write(e, self.path()),
         })
@@ -53,12 +56,12 @@ pub trait WriteableVirtualSensor<T: Raw>: VirtualSensor<T> {
 }
 
 #[cfg(feature = "writeable")]
-impl<T: Raw> WriteableVirtualSensor<T> for VirtualSensorStruct {}
+impl<T: Raw + Sync> AsyncWriteableVirtualSensor<T> for VirtualSensorStruct {}
 
 /// Creates a virtual sensor from the given file at `path`.
 pub fn virtual_sensor_from_path<T: Raw>(
     path: impl Into<PathBuf>,
-) -> Result<impl VirtualSensor<T> + Clone + Send + Sync> {
+) -> Result<impl AsyncVirtualSensor<T> + Clone + Send + Sync> {
     let path = path.into();
 
     if !path.is_file() {
@@ -73,9 +76,9 @@ pub fn virtual_sensor_from_path<T: Raw>(
 
 #[cfg(feature = "writeable")]
 /// Creates a virtual sensor from the given file at `path`.
-pub fn writeable_virtual_sensor_from_path<T: Raw>(
+pub fn writeable_virtual_sensor_from_path<T: Raw + Sync>(
     path: impl Into<PathBuf>,
-) -> Result<impl WriteableVirtualSensor<T> + Clone + Send + Sync> {
+) -> Result<impl AsyncWriteableVirtualSensor<T> + Clone + Send + Sync> {
     let path = path.into();
 
     if !path.is_file() {
